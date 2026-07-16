88a35cfd63a0bac71cab4858fd952681a69f53d1
natalie <natgiselle29@gmail.com>
2026-07-16 00:31:42 -0400

    implements DOM elements and query selectors

---
+++ script.js
@@ -0,0 +1,9 @@
+/** (Phase 0) DOM REFERENCES: 
+ * use document.querySelector(blank) for those without id
+ * document.getElementById(blank) for #results
+ * 
+*/
+
+const searchBarInput = document.getElementById("search-bar-input");
+const searchBarBtn = document.getElementById("search-bar-btn");
+const results = document.getElementById("results");
